34484cf4b21ffb8ad7be6e66e38214dd923381d2
rebels-community <rebelsbotofficial@gmail.com>
2024-04-22 23:08:51 +0300

    second commit

--- zad2/routes/index.js
+++ zad2/routes/index.js
@@ -2,6 +2,7 @@ const home = require('../views/home');
 const car = require('../views/car');
 const { renderPage } = require('../views/car');
 const addCar = require('../views/add-car');
+const querystring = require('querystring');
 
 const carInfo = { marka: 'Ford', model: 'Mustang' };
 
@@ -10,10 +11,23 @@ module.exports = {
         home(req, res);
     },
     handleAddCar: (req, res) => {
-        // if (req.method == 'GET') {
-        addCar(req, res);
-        return;
+        if (req.method == 'GET') {
+            addCar(req, res);
+            return;
+        }
 
+        if (req.method == 'POST') {
+            let body = '';
+            req.on('data', (chunk) => {
+                body += chunk;
+            });
+            req.on('end', () => {
+                const formData = querystring.parse(body);
+                carInfo.marka = formData.make;
+                carInfo.model = formData.model;
+            });
+            home(req, res);
+        }
     },
     handleCar: (req, res) => {
         const html = renderPage(JSON.stringify(carInfo));
